dd1abfb7de24239fe0107a261628d3161f6aefb3
Aline Rodrigues <138261288+Alinersl@users.noreply.github.com>
2026-04-06 15:29:56 -0300

    Algumas mudanças no site

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,35 +1,24 @@
-const comentarios = [];
-const form = document.querySelector('#form');
-const listaComentarios = document.querySelector('#listaComentarios');
+//carrosel//
+let index = 0;
+showSlide(index);
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const nome = form.querySelector('#nome').value;
-    const email = form.querySelector('#email').value;
-    const mensagem = form.querySelector('#mensagem').value;
-
-    const comentario = { nome, email, mensagem }
-    comentarios.push(comentario);
-
-    adicionarComentario();
-    form.reset();
-});
-function adicionarComentario() {
-    listaComentarios.innerHTML = '';
-    for (let i = 0; i < comentarios.length; i++) {
-        const comentario = comentarios[i];
-        const card = document.createElement('div');
-        card.classList.add('card');
-        listaComentarios.innerHTML += `
-            <div>
-                <h3 style= color: #f0f0f0;> Nome: ${comentario.nome}</h3>
-                <p style= color: #f0f0f0;> Email: ${comentario.email}</p>
-            </div>
-            <p style= color: #f0f0f0;> Comentou: <span> ${comentario.mensagem} </span></p><hr>`;
-    }
+function showSlide(i) {
+  let slides = document.getElementsByClassName("slide");
+  
+  if (i >= slides.length) index = 0;
+  if (i < 0) index = slides.length - 1;
+  
+  for (let s of slides) {
+    s.style.display = "none";
+  }
+  
+  slides[index].style.display = "block";
 }
 
+function changeSlide(n) {
+  index += n;
+  showSlide(index);
+}
 
 const botaoDark = document.getElementById('mudar-dark');
 
@@ -51,3 +40,36 @@ botaoDark.addEventListener('click', function () {
     botaoDark.textContent = '🌙 Modo Escuro';
   }
 });
+const comentarios = [];
+const form = document.querySelector('#form');
+const listaComentarios = document.querySelector('#listaComentarios');
+
+// corrigir
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nome = form.querySelector('#nome').value;
+    const email = form.querySelector('#email').value;
+    const mensagem = form.querySelector('#mensagem').value;
+
+    const comentario = { nome, email, mensagem }
+    comentarios.push(comentario);
+
+    adicionarComentario();
+    form.reset();
+});   
+
+function adicionarComentario() {
+    listaComentarios.innerHTML = '';
+    for (let i = 0; i < comentarios.length; i++) {
+        const comentario = comentarios[i];
+        const card = document.createElement('div');
+        card.classList.add('card');
+        listaComentarios.innerHTML += `
+            <div>
+                <h3 style= color: #f0f0f0;> Nome: ${comentario.nome}</h3>
+                <p style= color: #f0f0f0;> Email: ${comentario.email}</p>
+            </div>
+            <p style= color: #f0f0f0;> Comentou: <span> ${comentario.mensagem} </span></p><hr>`;
+    }
+}
