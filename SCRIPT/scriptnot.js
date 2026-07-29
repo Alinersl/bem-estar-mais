cf64788e76e0af7941f5507d34f076f530928b7d
Ana08Julia <estudanteaj@gmail.com>
2026-07-29 15:35:17 -0300

    atualizando site

--- SCRIPT/scriptnot.js
+++ SCRIPT/scriptnot.js
@@ -1,31 +1,24 @@
 // MODO CLARO E ESCURO
-const botaoDark = document.getElementById('mudar-dark');
+// const botaoDark = document.getElementById('mudar-dark');
 
-if (localStorage.getItem('modo') === 'dark') {
-  document.body.classList.add('dark');
-  botaoDark.textContent = '☀️ Modo Claro';
-} else {
-  botaoDark.textContent = '🌙 Modo Escuro';
-}
+// if (localStorage.getItem('modo') === 'dark') {
+//   document.body.classList.add('dark');
+//   botaoDark.textContent = '☀️ Modo Claro';
+// } else {
+//   botaoDark.textContent = '🌙 Modo Escuro';
+// }
 
-botaoDark.addEventListener('click', function () {
-  document.body.classList.toggle('dark');
+// botaoDark.addEventListener('click', function () {
+//   document.body.classList.toggle('dark');
 
-  if (document.body.classList.contains('dark')) {
-    localStorage.setItem('modo', 'dark');
-    botaoDark.textContent = '☀️ Modo Claro';
-  } else {
-    localStorage.setItem('modo', 'light');
-    botaoDark.textContent = '🌙 Modo Escuro';
-  }
-});
-
-// BANNERS
-const logoLink = document.getElementById("logoLink");
-const paginaAtual = window.location.pathname.split("/").pop();
-
-logoLink.href = paginaAtual;
-
+//   if (document.body.classList.contains('dark')) {
+//     localStorage.setItem('modo', 'dark');
+//     botaoDark.textContent = '☀️ Modo Claro';
+//   } else {
+//     localStorage.setItem('modo', 'light');
+//     botaoDark.textContent = '🌙 Modo Escuro';
+//   }
+// });
 
 // BOTAO FAVORITO
 const toast = document.getElementById("toast");
